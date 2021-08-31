9d29472f9c7b1575d31adb9e1685e1ef0bac7651
FloValdes <ftvaldes1@uc.cl>
2021-08-31 18:47:52 -0400

    add functionality

---
+++ app/views/applications/_application.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! application, :id, :intention, :quantity, :timestamp, :product_id, :created_at, :updated_at
+json.url application_url(application, format: :json)
---
+++ test/system/applications_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class ApplicationsTest < ApplicationSystemTestCase
+  setup do
+    @application = applications(:one)
+  end
+
+  test "visiting the index" do
+    visit applications_url
+    assert_selector "h1", text: "Applications"
+  end
+
+  test "creating a Application" do
+    visit applications_url
+    click_on "New Application"
+
+    fill_in "Intention", with: @application.intention
+    fill_in "Product", with: @application.product_id
+    fill_in "Quantity", with: @application.quantity
+    fill_in "Timestamp", with: @application.timestamp
+    click_on "Create Application"
+
+    assert_text "Application was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Application" do
+    visit applications_url
+    click_on "Edit", match: :first
+
+    fill_in "Intention", with: @application.intention
+    fill_in "Product", with: @application.product_id
+    fill_in "Quantity", with: @application.quantity
+    fill_in "Timestamp", with: @application.timestamp
+    click_on "Update Application"
+
+    assert_text "Application was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Application" do
+    visit applications_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Application was successfully destroyed"
+  end
+end
