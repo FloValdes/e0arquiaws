13c12f76db0e8085a3e870adb914b93210c33a14
FloValdes <ftvaldes1@uc.cl>
2021-08-31 15:24:14 -0400

    fix ssl_renew.sh

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ubuntu/e0arquiaws/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP app
+$COMPOSE run certbot renew --dry-run && $COMPOSE exec nginx -s reload
 $DOCKER system prune -af
